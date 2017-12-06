06fee47b8adc769d7f56c21cb81557ffc9b3df1c
Jasper Ho <jzpero@users.noreply.github.com>
2017-12-06 18:48:01 -0500

    Working Twitter Bot

--- bot.py
+++ bot.py
@@ -1,33 +1,42 @@
 from secrets import *
 
 import tweepy
-import re
 from textblob import TextBlob
 from datetime import datetime
+import json
+import haikubot
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 class MyStreamer(tweepy.StreamListener):
-    def on_status(self, status):
-        if status.retweeted:
-            return
-        text = cleanTweet(status.text)
-        print text
-        blob = TextBlob(text)
+    def __init__(self):
+        print "Init"
+
+    def on_connect(self):
+        print "Connected"
+
+    def on_disconnect(self, status):
+        print "Disconnected"
 
     def on_error(self, status_code):
         if status_code == 420:
             return False
 
-
-def cleanTweet(tweet):
-        # Remove Links, Special Characters etc from tweet
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w +:\ / \ / \S +)", " ", tweet).split())
-
+    def on_data(self, data):
+        decoded = json.loads(data)
+        if decoded.has_key('direct_message'):
+            dm = decoded['direct_message']
+            dm_text = dm['text']
+            dm_sender = dm['sender_screen_name']
+            if dm_sender != 'hai_cudi':
+                print dm_sender,'asks for',dm_text
+                haiku = haikubot.make_haiku(dm_text)
+                api.send_direct_message(screen_name=dm_sender,text=haiku)
+    
 
 if __name__ == '__main__':
     myStreamListener = MyStreamer()
     stream = tweepy.Stream(auth, myStreamListener)
-    stream.filter(track=['Canada', '@JustinTrudeau', 'Toronto'], async=True)
+    stream.userstream()
--- haikubot.py
+++ haikubot.py
@@ -4,30 +4,47 @@ from textstat.textstat import textstat
 import re
 import random
 
-def make_haiku(artist):
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers = {'User-Agent': user_agent, }
-    req = urllib2.Request('https://genius.com/artists/{}'.format(artist.replace(' ', '-')), headers=headers)
+from nltk.corpus import cmudict
+d = cmudict.dict()
+
+
+def nsyl(word):
+    if d.has_key(word.lower()):
+        return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]][0]
+    return textstat.syllable_count(word)
+
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+headers = {'User-Agent': user_agent, }
+
+def get_artist_song_links(artist):
+    req = urllib2.Request(
+        'https://genius.com/artists/{}'.format(artist.replace(' ', '-')), headers=headers)
     response = urllib2.urlopen(req)
     soup = BeautifulSoup(response, 'html.parser')
-    #with open('soup.txt','w') as out:
-    #    out.write(soup.prettify().encode('utf-8'))
-
     artist_song_links = []
     for card in soup.findAll('a', {'class': 'mini_card'}):
         artist_song_links.append(card.get('href'))
-    #print artist_song_links
+    return artist_song_links
+
+def get_lyrics(song_page):
+    song_req = urllib2.Request(song_page, headers=headers)
+    song_response = urllib2.urlopen(song_req)
+    html = BeautifulSoup(song_response, 'html.parser')
+    [h.extract() for h in html('script')]
+    lyrics = html.find("div", class_="lyrics").get_text().encode('utf-8').strip().split('\n')
+    return lyrics
+
+def make_haiku(artist):
+    try:
+        artist_song_links = get_artist_song_links(artist)
+    except:
+        return "Sorry. No haiku or lyrics found."
 
     five_list = []
     seven_list = []
 
     for song_page in artist_song_links:
-        #print song_page
-        song_req = urllib2.Request(song_page, headers=headers)
-        song_response = urllib2.urlopen(song_req)
-        html = BeautifulSoup(song_response, 'html.parser')
-        [h.extract() for h in html('script')]
-        lyrics = html.find("div",class_="lyrics").get_text().encode('utf-8').strip().split('\n')
+        lyrics = get_lyrics(song_page)
         for line in lyrics:
             bad = False
             #for profanity in profanities:
@@ -42,18 +59,26 @@ def make_haiku(artist):
                 words = re.findall(u'[A-z\']+',line)
                 n = 0
                 for word in words:
-                    n += textstat.syllable_count(word)
+                    n += nsyl(word)                
+                source = song_page
+
                 #print line, n
                 if n == 5:
-                    five_list.append((line, n))
+                    five_list.append((line, n, source))
                 if n == 7:
-                    seven_list.append((line,n))
-
+                    seven_list.append((line, n, source))
+    
+    
     if len(five_list) >= 2 and len(seven_list) >= 1:
         five_pick_one = random.randint(0,len(five_list)-1)
         five_pick_two = random.randint(0, len(five_list) - 1)
         while five_pick_two == five_pick_one:
             five_pick_two = random.randint(0, len(five_list) - 1)
-        return '\n'.join([five_list[five_pick_one][0], seven_list[random.randint(0, len(seven_list) - 1)][0], five_list[five_pick_two][0]])
 
-print make_haiku("Gallant")
+        first_line = five_list[five_pick_one][0]
+        second_line = seven_list[random.randint(0, len(seven_list) - 1)][0]
+        third_line = five_list[five_pick_two][0]
+
+        haiku = '\n'.join([first_line, second_line, third_line])
+        return haiku
+    return "Sorry. No haiku or lyrics found."
