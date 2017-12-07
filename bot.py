36d0d9fe6b7c222cee8a627f6369b6be36f14dbd
Jasper Ho <jzpero@users.noreply.github.com>
2017-12-07 00:50:38 -0500

    Blocked a certain user lol

--- bot.py
+++ bot.py
@@ -23,22 +23,29 @@ class MyStreamer(tweepy.StreamListener):
             return False
 
     def on_data(self, data):
+        exclusion_list = ['938597347046580224', 'haikus420']
         decoded = json.loads(data)
         if decoded.has_key('direct_message'):
             dm = decoded['direct_message']
+            dm_sender_id = dm['sender_id_str']
             dm_text = dm['text']
             dm_sender = dm['sender_screen_name']
             if dm_sender != 'hai_cudi':
-                if dm_sender != 'jzpero':
-                    print dm_sender,'asks for',dm_text
-                    haiku = haikubot.make_haiku(dm_text)
-                    api.send_direct_message(screen_name=dm_sender,text=haiku)
-                else:
+                if dm_sender_id in exclusion_list or dm_sender in exclusion_list:
                     text = 'Sorry. No haiku or lyrics found.'
                     api.send_direct_message(screen_name=dm_sender, text=text)
+                else:
+                    print dm_sender, 'asks for', dm_text
+                    haiku = haikubot.make_haiku(dm_text)
+                    api.send_direct_message(screen_name=dm_sender, text=haiku)
+                    
     
 
 if __name__ == '__main__':
+    with open('all_dms.txt','w') as out:
+        dms = api.direct_messages()
+        for dm in dms:
+            out.write(str(dm))
     myStreamListener = MyStreamer()
     stream = tweepy.Stream(auth, myStreamListener)
     stream.userstream()
