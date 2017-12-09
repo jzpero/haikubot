bfc79c10ad446ce62f9ae07b8185ab8cd0778f12
Jasper Ho <jzpero@users.noreply.github.com>
2017-12-08 21:27:20 -0500

    Delete secrets.py

--- secrets.py
+++
@@ -1,4 +0,0 @@
-consumer_key = '3SMTUqrDGNR2txtcOxjIcOyXI'
-consumer_secret = '54W9sSkFTF5xO5rub7z6sUkCGUKozIdOCzXpts52gtEnp2GPcr'
-access_token = '938222820504379393-j2ovaqbB0t8W8Hf6DwcZ5GZkcXL0jAE'
-access_secret = 'suHBJfF63meKADY9lr0AJGhAYKQRyFwuDj3WIrlTCFwAR'
